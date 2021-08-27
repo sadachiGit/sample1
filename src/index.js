4f47a1c7ed8b7cd93e063ca292d4186ff9e245ca
sadachiGit <89511389+sadachiGit@users.noreply.github.com>
2021-08-27 17:44:39 +0900

    １８

--- src/index.js
+++ src/index.js
@@ -1,10 +1,45 @@
-import "./styles.css";
+/**
+ * const, let
+ */
+var val01 = "hogehoge";
+console.log(val01);
+// var 上書き可能
+val01 = "over over!!";
+console.log(val01);
+// var 再宣言
+var val01 = "hogehoge2222";
+console.log(val01);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+//
+let val02 = "変数22";
+console.log(val02);
+// let 上書き可能
+val02 = "hogehoge222";
+console.log(val02);
+// let 再宣言はだめ
+// let val02 = "aaa";
+
+// const 上書き・再宣言不可
+const val03 = "aaaaa";
+console.log(val03);
+// val03 = 'bbbbbb';
+
+// object
+const val04 = {
+  name: "aaaaa",
+  age: 30
+};
+console.log(val04);
+// objectは データの中身を更新できる
+val04.name = "bbbbb";
+console.log(val04);
+// constで定義したオブジェクトは
+// プロパティの変更が可能
+
+const val05 = ["dog", "cat"];
+console.log(val05);
+val05[0] = "bbbbbb";
+console.log(val05);
+val05.push("ggggg");
+console.log(val05);
+// object は 基本 const を使う
